42c55af7b54777c6640b4d5e1f4382ff584fcbce
Milu László <milu_laci@milu.laci-HP-ProBook-6560b>
2016-05-30 12:02:21 +0200

    fizz buzz working

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,20 @@
 def fizzbuzz(number):
-    return
+
+    to_return = ""
+    if number % 3 == 0:
+        to_return += "Fizz"
+    if number % 5 == 0:
+        to_return += "Buzz"
+    if not to_return:
+        to_return = number
+    return to_return
 
 
 def main():
+    num = ""
+    for i in range(1, 101):
+        num = fizzbuzz(i)
+        print(num)
     return
 
 if __name__ == '__main__':
