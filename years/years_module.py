bc896bbc660c746e9c7413802b2137dc31da5419
arvisura87 <jakab.tamas@me.com>
2016-05-30 14:44:39 +0200

    done

--- years/years_module.py
+++ years/years_module.py
@@ -1,11 +1,18 @@
-import datetime
+import time
 
 
 def years(age):
-    return
+    diff = 100 - age
+    current_year = int((time.strftime("%Y")))
+    return current_year + diff
 
 
 def main():
+    name = input("Whats your name? ")
+    age = int(input("What's your age? "))
+
+    print("You will be 100 years old in", years(age))
+
     return
 
 
