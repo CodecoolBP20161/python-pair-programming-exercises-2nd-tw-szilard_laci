774a67b36b9b26b0835aa472403f0964e4ef870c
Milu László <milu_laci@milu.laci-HP-ProBook-6560b>
2016-05-31 10:16:24 +0200

    test

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -10,8 +10,7 @@ def listoverlap(list1, list2):
 def main():
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    print(listoverlap(a,b))
-    return
+    print(listoverlap(a, b))
 
 
 if __name__ == '__main__':
