9f1d0eb054f6dafbc0216441c0e048a2c3397592
Milu László <hockeylaci@gmail.com>
2016-06-01 09:44:55 +0200

    able to handle spaces and uppercase letters in the given text

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,19 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    string = string.replace(" ", "")
+    string = string.lower()
+    if string == string[::-1]:
+        return True
+    else:
+        return False
 
 
 def main():
-    return
+    text = input("Enter text: ")
+    if palindrome(text):
+        print(text, "IS a palindrome")
+    else:
+        print(text, "is NOT a palindrome")
+
 
 
 if __name__ == '__main__':
