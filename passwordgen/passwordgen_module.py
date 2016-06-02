b7f82d887bd0a2969b42d8e0ed1a223b84935a89
Milu László <hockeylaci@gmail.com>
2016-06-02 11:12:20 +0200

    able to generate hard passwords

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,9 +1,34 @@
 def passwordgen():
-    return
+    from random import randrange
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "!@#$%^&*()?"
+    chars = lowercase + uppercase + numbers + symbols
+    let_it_go = False
+    while let_it_go is False:
+        pw = ""
+        lowc_bool = False
+        upc_bool = False
+        numbool = False
+        symbool = False
+        for i in range(8):
+            char_to_insert = chars[(randrange(1, len(chars)))]
+            pw = pw[:len(pw)] + char_to_insert
+            if char_to_insert in lowercase:
+                lowc_bool = True
+            if char_to_insert in uppercase:
+                upc_bool = True
+            if char_to_insert in numbers:
+                numbool = True
+            if char_to_insert in symbols:
+                symbool = True
+        if lowc_bool is True and upc_bool is True and numbool is True and symbool is True:
+            return pw
 
 
 def main():
-    return
+    print(passwordgen())
 
 
 if __name__ == '__main__':
